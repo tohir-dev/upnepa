e886ff06b232c24231a97bc6293624e98ed8e1c7
tohir <tohirbabs@gmail.com>
2025-03-09 19:04:24 +0100

    shadcn map packages

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-animate"), // Ensure this line is included
+  ],
+};
